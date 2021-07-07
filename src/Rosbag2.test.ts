8cd7ccc64fb9266b1879572467b3ae9fbd0a47b7
John Hurliman <jhurliman@jhurliman.org>
2021-07-06 18:27:34 -0700

    Remove sqlite testing for now

--- src/Rosbag2.test.ts
+++ src/Rosbag2.test.ts
@@ -2,9 +2,7 @@ import { Time, add as addTimes, isGreaterThan, isTimeInRangeInclusive } from "@f
 import path from "path";
 
 import { Rosbag2 } from "./Rosbag2";
-import { SqliteNodejs } from "./nodejs";
-import { open } from "./nodejs/open";
-import { SqliteSqljs } from "./web";
+import { SqliteNodejs, open as openNodejs } from "./nodejs";
 
 const BAG_START: Time = { sec: 1585866235, nsec: 112411371 };
 const BAG_END: Time = { sec: 1585866239, nsec: 643508139 };
@@ -19,7 +17,7 @@ describe("SqliteNodejs single bag handling", () => {
 
   it("reads messages", async () => {
     const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
+    const bag = await openNodejs(bagPath);
 
     expect(bag.baseDir).toEqual(bagPath);
     expect(bag.files.size).toEqual(4);
@@ -55,7 +53,7 @@ describe("SqliteNodejs single bag handling", () => {
 
   it("reads start/end times", async () => {
     const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
+    const bag = await openNodejs(bagPath);
 
     expect(bag.metadata).toBeDefined();
     expect(bag.metadata!.startingTime).toEqual(BAG_START);
@@ -68,7 +66,7 @@ describe("SqliteNodejs single bag handling", () => {
 
   it("reads the topic list", async () => {
     const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
+    const bag = await openNodejs(bagPath);
 
     const topics = await bag.readTopics();
     expect(topics.length).toEqual(3);
@@ -79,84 +77,7 @@ describe("SqliteNodejs single bag handling", () => {
 
   it("reads the topic list", async () => {
     const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
-
-    const counts = await bag.messageCounts();
-    expect(counts.size).toEqual(2);
-    expect(counts.get("/rosout")).toEqual(10);
-    expect(counts.get("/topic")).toEqual(10);
-  });
-});
-
-describe("SqliteSqljs single bag handling", () => {
-  it("fails when missing metadata.yaml", async () => {
-    const bag = new Rosbag2(".", [], (fileEntry) => new SqliteSqljs(fileEntry.file));
-    await expect(bag.open()).rejects.toBeDefined();
-  });
-
-  it("reads messages", async () => {
-    const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
-
-    expect(bag.baseDir).toEqual(bagPath);
-    expect(bag.files.size).toEqual(4);
-    expect(bag.files.get("metadata.yaml")).toBeDefined();
-    expect(bag.files.get("talker.db3")).toBeDefined();
-
-    let seenRosout = false;
-    let seenTopic = false;
-    let prevTime: Time = { sec: 0, nsec: 0 };
-    for await (const msg of bag.readMessages()) {
-      expect(isTimeInRangeInclusive(msg.timestamp, BAG_START, BAG_END)).toEqual(true);
-      expect(isGreaterThan(msg.timestamp, prevTime)).toEqual(true);
-      expect(TOPICS.includes(msg.topic.name)).toEqual(true);
-      expect(DATATYPES.includes(msg.topic.type)).toEqual(true);
-      if (!seenRosout && msg.topic.name === "/rosout") {
-        seenRosout = true;
-        expect(msg.data).toEqual({
-          stamp: { sec: 1585866235, nsec: 112130688 },
-          level: 20,
-          name: "minimal_publisher",
-          msg: "Publishing: 'Hello, world! 0'",
-          file: "/opt/ros2_ws/eloquent/src/ros2/examples/rclcpp/minimal_publisher/lambda.cpp",
-          function: "operator()",
-          line: 38,
-        });
-      } else if (!seenTopic && msg.topic.name === "/topic") {
-        seenTopic = true;
-        expect(msg.data).toEqual({ data: "Hello, world! 0" });
-      }
-      prevTime = msg.timestamp;
-    }
-  });
-
-  it("reads start/end times", async () => {
-    const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
-
-    expect(bag.metadata).toBeDefined();
-    expect(bag.metadata!.startingTime).toEqual(BAG_START);
-    expect(addTimes(bag.metadata!.startingTime!, bag.metadata!.duration!)).toEqual(BAG_END);
-
-    const [startTime, endTime] = await bag.timeRange();
-    expect(startTime).toEqual(BAG_START);
-    expect(endTime).toEqual(BAG_END);
-  });
-
-  it("reads the topic list", async () => {
-    const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
-
-    const topics = await bag.readTopics();
-    expect(topics.length).toEqual(3);
-    expect(topics[0]!.name).toEqual("/rosout");
-    expect(topics[1]!.name).toEqual("/parameter_events");
-    expect(topics[2]!.name).toEqual("/topic");
-  });
-
-  it("reads the topic list", async () => {
-    const bagPath = path.join(__dirname, "..", "tests", "bags", "talker");
-    const bag = await open(bagPath);
+    const bag = await openNodejs(bagPath);
 
     const counts = await bag.messageCounts();
     expect(counts.size).toEqual(2);
